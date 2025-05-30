c1b9d52bc998ca084ab523c8a87956d1468dc796
Maximiliano Ariel Giovine <maxiagiovine@gmail.com>
2025-05-30 14:59:34 -0300

    routin commit

--- OneDrive/Escritorio/essential-web-corp/src/pages/Projects.jsx
+++ OneDrive/Escritorio/essential-web-corp/src/pages/Projects.jsx
@@ -1,7 +1,32 @@
 export default function Projects() {
   return (
-    <div className="projects">
-      <h1>Proyectos</h1>
-      <p>Aquí hay algunos de mis proyectos recientes:</p>
-    </div>);
+  <>
+    <div className="projects-title">
+      <h1>Nuestros Proyectos</h1>
+      <h2>Un poco de nuestro Portfolio</h2>
+      <p>En Essential Web Corp entendemos que un portfolio actualizado y la experiencia visible y comprobable es vital para generar
+        un lazo de confianza con nuestros clientes. Por eso, a continuación te mostramos algunos de nuestros proyectos más destacados y novedosos.
+      </p>
+      <p>Además, te dejamos algunos de los testimonios de nuestros clientes satisfechos con nuestra labor</p>
+    </div>
+    <div className="projects-container">
+      <div className="project-card">
+        <h3>Proyecto Maria Paz</h3>
+        <p>Este fue el primer proyecto que afrontamos. Para llevarlo a cabo nos centramos en el uso de tecnologías de TiendaNube para la confexión y diseños a medida
+          se implementaron pasarelas de pago y estrategias de marketing digital y SEO para mejorar la visibilidad y alcance del negocio. De ser una empresa local con
+          alcance limitadoa la ciudad donde se encuentra, hoy en día gracias a su página web y su presencia en redes, logro un aproach a nivel nacional y un crecimiento en
+          sus ventas.
+        </p>
+      </div>
+      <div className="project-card">
+        <h3>Proyecto 2</h3>
+        <p>Descripción breve del proyecto 2.</p>
+      </div>
+      <div className="project-card">
+        <h3>Proyecto 3</h3>
+        <p>Descripción breve del proyecto 3.</p>
+      </div>
+    </div>
+  </>
+  )
 }
