342c7bc86a9c377c7d00ab8f4088a6a898c7acf9
Maximiliano Ariel Giovine <maxiagiovine@gmail.com>
2025-05-30 13:33:18 -0300

    documentación y privatización de las variables de entorno

--- OneDrive/Escritorio/Repositorio Estudio/BackendPlataforma/Index.js
+++ OneDrive/Escritorio/Repositorio Estudio/BackendPlataforma/Index.js
@@ -3,6 +3,18 @@ const connectDB = require('./src/config/dbcon');
 
 const PORT = process.env.PORT || 3000;
 
+/*
+    este es el entry point del backend
+    index se encarga de iniciar el servidor, hacer la conexion con la base de datos y que todo funcione
+    cualquier error en la conexion a la base de datos debe revisarse aqui.
+    si el error de conexión no está aqui, buscar en dbcon.js
+    este modulo es el encargado de conectar la base de datos MongoDB a la aplicacion
+    cualquier error en solicitudes HTTP debe revisarse en App.js o dentro de las rutas.
+    si en las rutas no se encuentra el error, buscar en los controladores
+    si en los controladores no se encuentra el error, buscar en los servicios
+    si en los servicios no se encuentra el error, buscar en los modelos
+    si en los modelos no se encuentra el error, buscar en los esquemas
+*/
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
     connectDB()
--- OneDrive/Escritorio/Repositorio Estudio/BackendPlataforma/src/server.js
+++ OneDrive/Escritorio/Repositorio Estudio/BackendPlataforma/src/server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 
+/*
+server se encarga de conectar con la librería de express y configurar las demas librerías que utiliza el proyecto con el backend
+el propósito es modularizar el código de arranque de la aplicación
+*/
 const app = express();
 // Middleware
 app.use(cors());
@@ -10,3 +14,4 @@ app.use(morgan("dev"));
 // Connect to MongoDB
 
 module.exports = app;
+// app se exporta al Index.js para ser utilizado en el arranque de la aplicación
