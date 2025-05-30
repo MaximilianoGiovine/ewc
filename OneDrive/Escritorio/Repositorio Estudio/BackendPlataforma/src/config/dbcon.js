52daa4247c5d7ddb6659b3927022c3d41c7a9378
Maximiliano Ariel Giovine <maxiagiovine@gmail.com>
2025-05-30 13:26:27 -0300

    conexión a la base de datos de MONGODB

--- OneDrive/Escritorio/Repositorio Estudio/BackendPlataforma/src/config/dbcon.js
+++ OneDrive/Escritorio/Repositorio Estudio/BackendPlataforma/src/config/dbcon.js
@@ -1,8 +1,17 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
+
+/* este modulo conecta todo el proyecto backend a la base de datos MongoDB
+esta diseñado para ser utilizado en el app y administrar todos los datos de la plataforma
+este modulo es el encargado de conectar la base de datos MongoDB a la aplicacion
+cualquier error en la conexión a la base de Datos debe revisarse aqui. */
 
 const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://GiovineMaxi:gwID2RmNrK1iscA7@basededatos.laxnihf.mongodb.net/')
+    await mongoose.connect(process.env.MONGODB_URI)
         .then(() => console.log("MongoDB connected successfully"))
         .catch(err => console.error("MongoDB connection error:", err));
 };
 module.exports = connectDB;
+
+//a la hora de hacer deploy, hay que reconfigurar la variable de entorno MONGODB_URI
+// para que apunte a la base de datos de produccion
